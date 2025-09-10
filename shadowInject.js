7da1c846f0f01e279f04de3f490823ad91c5dc18
erichung9060 <rthung96@gmail.com>
2025-09-10 16:22:46 +0800

    feat: support script injection without refreshing page

--- shadowInject.js
+++ shadowInject.js
@@ -1,3 +1,7 @@
-const injectedScript = document.createElement('script');
-injectedScript.src = chrome.runtime.getURL('injected.js');
-(document.head || document.documentElement).appendChild(injectedScript);
+if (!Element.prototype._attachShadow) {
+    console.log("Overriding attachShadow to use open mode");
+    Element.prototype._attachShadow = Element.prototype.attachShadow;
+    Element.prototype.attachShadow = function() {
+        return this._attachShadow({ mode: "open" });
+    };
+}
